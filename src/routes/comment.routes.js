1addbb3b6177876dfa7ebe5b856294004f6ec6de
Erwin <erwinb1996@gmail.com>
2021-01-07 22:04:36 -0500

    [ADD] swagger for user controller added

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
-
+const { AuthMiddleware } = require("../middlewares");
 module.exports = function ({ CommentController }) {
   const router = Router();
   router.get("/:ideaId", CommentController.getIdeaComments);
   router.get("/:commentId/unique", CommentController.get);
-  router.patch("/:commentId", CommentController.update);
-  router.delete("/:commentId", CommentController.delete);
-  router.post("/:ideaId", CommentController.createComment);
+  router.patch("/:commentId", AuthMiddleware, CommentController.update);
+  router.delete("/:commentId", AuthMiddleware, CommentController.delete);
+  router.post("/:ideaId", AuthMiddleware, CommentController.createComment);
   return router;
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,9 @@ const compression = require("compression");
 require("express-async-errors");
 
 const { NotFoundMiddleware, ErrorMiddleware } = require("../middlewares");
+const swaggerUI = require("swagger-ui-express");
+const { SWAGGER_PATH } = require("../config");
+const swaggerDocument = require(SWAGGER_PATH);
 
 module.exports = function ({
   HomeRoutes,
@@ -27,6 +30,7 @@ module.exports = function ({
   apiRoutes.use("/auth", AuthRoutes);
 
   router.use("/v1/api", apiRoutes);
+  router.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerDocument))
 
   router.use(NotFoundMiddleware);
   router.use(ErrorMiddleware);
